f2d2119687b6c9eb0bf157d559192b9609082cc0
Chey04 <vchukwukere123@gmail.com>
2023-03-08 07:52:45 -0800

    Added files

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     lenght = len(str)
     parse = 0
     for ch in range(lenght):
-        if (str[parse] <= 'A'  and str[parse] >= 'Z'):
+        if (str[parse] <= 'A' and str[parse] >= 'Z'):
             continue
         else:
             print("{}".format(chr(ord(str[parse]) - 32)), end="")
