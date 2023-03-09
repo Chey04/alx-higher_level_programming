b480e6b12774ff1168d2848eff75924c92312838
Chey04 <vchukwukere123@gmail.com>
2023-03-09 11:20:15 -0800

    Added files

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import add_0
+if __name__ == "__main__"
 
-a = 1
-b = 2
-total = add_0.add(a, b)
+    from add_0 import add.py
 
-print("{} + {} = {}".format(a, b, total))
+    a = 1
+    b = 2
+    total = add_0.add(a, b)
+
+    print("{} + {} = {}".format(a, b, total))
