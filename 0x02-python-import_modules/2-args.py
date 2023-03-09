4114aecd51838e13c023242090c269613c4edb1b
Chey04 <vchukwukere123@gmail.com>
2023-03-09 12:45:07 -0800

    Added files

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,12 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("0 arguments")
 
-    else:
-        print("{} arguments:" .format((len(argv) - 1), end=" "))
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1: {}" .format(argv[1]))
 
-        for i in range(1, (len(argv))):
+    else:
+        print("{} arguments:" .format(len(argv) - 1))
+
+        for i in range(1, len(argv)):
             print("{}: {}" .format(i, argv[i]))
