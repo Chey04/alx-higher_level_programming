4f49cd792e133cebabe2a47eeef143a4e9ac7fda
Chey04 <vchukwukere123@gmail.com>
2023-03-28 12:46:36 -0700

    Added files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defining a class: square"""
+
+
+class Square:
+    """initializing class fields"""
+
+    def __init__(self, size):
+        """initializing class fields with args
+
+        Args:
+            size (int): size of square
+        """
+        self._Square__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defining a class: Square"""
+
+
+class Square:
+    """created a class called square"""
+
+    def __init__(self, size=0):
+        """initializing some attributes
+
+        Args:
+            size (int): size of square
+        """
+
+        if not type(size) is int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self._square__size = size
