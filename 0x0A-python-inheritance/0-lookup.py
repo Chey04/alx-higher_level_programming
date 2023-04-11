609dc45c5b63ad25e12d2e84fc5fc1031e62a2a9
Chey04 <vchukwukere123@gmail.com>
2023-04-11 12:24:28 -0700

    Added files

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
-#/usr/bin/python3
-''' The lookup function for an attribute'''
+#!/usr/bin/python3
+'''The lookup function for an attribute'''
+
 
 def lookup(obj):
-    """Returns all attribute of an object"""
+    '''Returns all attribute of an object'''
     return(dir(obj))
